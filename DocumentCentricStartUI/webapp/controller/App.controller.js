1eb4284e8d68d2cac2ca00def080ecea3ef182d6
GokhanYilmazturk <gokhan.yilmazturk@itelligence.com.tr>
2020-12-25 18:43:57 +0000

    first commit

---
+++ DocumentCentricStartUI/webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "./BaseController"
+],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (BaseController) {
+        "use strict";
+
+        return BaseController.extend("com.sap.bpm.DocumentCentricStartUI.controller.App", {
+            onInit: function () {
+                this.fnInitializeApp();
+                this.getContentDensityClass();
+            }
+        });
+    });
